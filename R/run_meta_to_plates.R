59729583eab60f441dde40106d7f28946e662afa
Adam Bennett <43841526+a4000@users.noreply.github.com>
2025-11-03 08:39:13 +0800

    Update run_meta_to_plates.R

--- R/run_meta_to_plates.R
+++ R/run_meta_to_plates.R
@@ -1,25 +1,29 @@
 source("R/meta_to_plates.R")
-input_file  <- "test_data/input/AB_V12_V9_metadata.xlsx"
+metadata    <- "test_data/input/AB_V12_V9_metadata.xlsx"
+index_file  <- "test_data/input/R_input_Template.xlsx"
 output_file <- "test_data/output/output_df.xlsx"
-assays      <- c("16S", "MiFish")
-run         <- "run1"
+assays      <- c("16S", "MiFishU", "MiFishE2", "COILeray")
 
 plates_to_skip <- list(
   "16S" = 0,
-  "MiFish" = 0
+  "MiFishU" = 0,
+  "MiFishE2" = 0,
+  "COILeray" = 0
 )
 samples_to_skip <- list(
   "16S" = 0,
-  "MiFish" = 0
+  "MiFishU" = 0,
+  "MiFishE2" = 0,
+  "COILeray" = 0
 )
 
 # strategy can be 'UDI' for unique dual-index, or 'UC' for unique combinatorial
 
 meta_to_plates(
-  input_file,
+  metadata,
+  index_file,
   output_file,
   assays,
-  run,
   skip_plates = plates_to_skip,
   skip_samples = samples_to_skip,
   strategy = "UC"
