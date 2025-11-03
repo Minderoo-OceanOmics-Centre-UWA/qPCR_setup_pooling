0d5620387b9bdc711bf93fef9d8032102ec265d4
Adam Bennett <43841526+a4000@users.noreply.github.com>
2025-11-03 08:42:20 +0800

    Update import_meta_df.R

--- R/import_meta_df.R
+++ R/import_meta_df.R
@@ -1,7 +1,7 @@
 import_meta_df <- function(excel_file, run) {
     excel_sheets <- excel_sheets(excel_file)
     meta_sheet   <- grep(
-        glob2rx("metadata"),
+        glob2rx("sampleMetadata"),
         excel_sheets,
         ignore.case = TRUE,
         value = TRUE
@@ -9,50 +9,61 @@ import_meta_df <- function(excel_file, run) {
 
     # Validate number of sheets called 'metadata'
     if (length(meta_sheet) == 0) {
-        stop("No sheets found nemed 'metadata'")
+        stop("No sheets found nemed 'sampleMetadata'")
     }
     if (length(meta_sheet) > 1) {
-        stop("Multiple sheets found named 'metadata'")
+        stop("Multiple sheets found named 'sampleMetadata'")
     }
 
     # Import metadata
-    meta_df   <- read_excel(excel_file, sheet = "metadata") %>%
+    meta_df   <- read_excel(excel_file, sheet = "sampleMetadata") %>%
         as.data.frame()
-    names(meta_df) <- gsub(" ", "", names(meta_df)) %>% {
-            gsub("_", "", .)
-        } %>%
-        toupper()
+    
+    num_rows_to_skip = 0
+    skip_rows = FALSE
+    for (row in rownames(meta_df)) {
+        num_rows_to_skip = num_rows_to_skip + 1
+        if (meta_df[row, 1] == "samp_name") {
+            skip_rows = TRUE
+            break
+        }
+    }
+    
+    if (skip_rows) {
+        meta_df   <- read_excel(excel_file, sheet = "sampleMetadata", skip = num_rows_to_skip) %>%
+            as.data.frame()
+    }
 
     # Validata metadata columns
-    if (!("SAMPLEID" %in% colnames(meta_df))) {
-        stop(paste0("Sheet 'metadata' must contain a 'sample_id' column"))
+    if (!("samp_name" %in% colnames(meta_df))) {
+        stop(paste0("Sheet 'sampleMetadata' must contain a 'samp_name' column"))
     }
 
     # Subset data if there is a sequencing run column
-    if ("SEQUENCINGRUN" %in% colnames(meta_df)) {
-        meta_df <- subset(meta_df, SEQUENCINGRUN == run)
+    if ("sequencingrun" %in% colnames(meta_df)) {
+        meta_df <- subset(meta_df, sequencingrun == run)
     }
 
     # Add empty columns
-    meta_df["FW_NO"]       <- NULL
-    meta_df["RV_NO"]       <- NULL
-    meta_df["FW_TAG"]      <- NULL
-    meta_df["RV_TAG"]      <- NULL
-    meta_df["FW_FULL_SEQ"] <- NULL
-    meta_df["RV_FULL_SEQ"] <- NULL
-    meta_df["ASSAY"]       <- NULL
-    meta_df["PLATE"]       <- NULL
-    meta_df["WELL"]        <- NULL
+    meta_df["fw_no"]       <- NULL
+    meta_df["rv_no"]       <- NULL
+    meta_df["fw_tag"]      <- NULL
+    meta_df["rv_tag"]      <- NULL
+    meta_df["fw_full_seq"] <- NULL
+    meta_df["rv_full_seq"] <- NULL
+    meta_df["assay"]       <- NULL
+    meta_df["plate"]       <- NULL
+    meta_df["well"]        <- NULL
 
     # Get sample ids
-    sample_ids             <- meta_df$SAMPLEID
+    sample_ids             <- meta_df$samp_name
 
     # Make sure there is no duplicate samples
     if (length(sample_ids) != length(unique(sample_ids))) {
         duplicates <- meta_df[
-            duplicated(meta_df$SAMPLE_ID) |
-            duplicated(meta_df$SAMPLE_ID, fromLast = TRUE),
-            "SAMPLE_ID"
+            duplicated(meta_df$samp_name) |
+            duplicated(meta_df$samp_name, fromLast = TRUE),
+            "samp_name"
         ]
         stop(
             paste0("You can't have duplicate sample ids. Duplicates: ",
