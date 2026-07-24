249da13d8a9cdca4314eef4ca239a909a5711f70
Adam Bennett <43841526+a4000@users.noreply.github.com>
2026-07-24 09:06:10 +0800

    Update run_meta_to_plates.R add prefix and output_dir

--- R/run_meta_to_plates.R
+++ R/run_meta_to_plates.R
@@ -1,20 +1,19 @@
 source("R/meta_to_plates.R")
 metadata    <- "test_data/input/AB_V12_V9_metadata.xlsx" # Use a vector of paths of you have multiple metadata files
 index_file  <- "input/index_Template.xlsx"
-output_file <- "output/output_df.xlsx"
-assays      <- c("16SFishD", "MiFishU", "MiFishE2", "COILeray")
+output_dir  <- "output/"
+assays      <- c("16SFishD", "MarVer1", "MiFishUE2")
+prefix      <- "" 
 
 plates_to_skip <- list(
   "16SFishD" = 0,
-  "MiFishU" = 0,
-  "MiFishE2" = 0,
-  "COILeray" = 0
+  "MarVer1" = 0,
+  "MiFishUE2" = 0
 )
 samples_to_skip <- list(
   "16SFishD" = 0,
-  "MiFishU" = 0,
-  "MiFishE2" = 0,
-  "COILeray" = 0
+  "MarVer1" = 0,
+  "MiFishUE2" = 0
 )
 
 # strategy can be 'UDI' for unique dual-index, or 'UC' for unique combinatorial
@@ -26,6 +25,7 @@ meta_to_plates(
   assays,
   skip_plates = plates_to_skip,
   skip_samples = samples_to_skip,
-  strategy = "UC"
+  strategy = "UC",
+  prefix = prefix
 )
 
